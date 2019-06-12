7034997f550e431559aa853dcef5568ce5fd795c
nalmutairi <40034790+nalmutairi@users.noreply.github.com>
2019-06-12 21:11:34 +0300

    Update socketStore.js

--- src/Stores/socketStore.js
+++ src/Stores/socketStore.js
@@ -1,1 +1,16 @@
 
+import socketIOClient from "socket.io-client";
+
+class SocketStore {
+  constructor() {
+    this.socket = socketIOClient("https://savemyspot-node.codeunicorn.io/");
+  }
+  restaurantSignIn(restaurantID) {
+    this.socket.emit("restaurant request", restaurantID);
+  }
+  seatGuest(queueID) {
+    this.socket.emit("seat guest", queueID);
+  }
+}
+
+export default new SocketStore();
